4c540f01cae00a28919594d02475742b54ad0b2e
Danish Hakim <danish.hakim04@gmail.com>
2026-08-18 19:54:10 +0800

    feat: add line numbers and intraline highlighting to the diff view

--- client/src/components/DiffView.svelte
+++ client/src/components/DiffView.svelte
@@ -13,8 +13,22 @@
 <div class="diff-view">
   {#each rows as row, i (i)}
     <div class="diff-view-row">
-      <div class="diff-view-cell" class:diff-removed={row.type === "changed" || row.type === "removed"}>{row.leftText ?? ""}</div>
-      <div class="diff-view-cell" class:diff-added={row.type === "changed" || row.type === "added"}>{row.rightText ?? ""}</div>
+      <div class="diff-view-gutter">{row.leftLine ?? ""}</div>
+      <div class="diff-view-cell" class:diff-removed={row.type === "changed" || row.type === "removed"}>
+        {#if row.leftSegments}
+          {#each row.leftSegments as seg, j (j)}<span class:diff-segment-changed={seg.changed}>{seg.text}</span>{/each}
+        {:else}
+          {row.leftText ?? ""}
+        {/if}
+      </div>
+      <div class="diff-view-gutter">{row.rightLine ?? ""}</div>
+      <div class="diff-view-cell" class:diff-added={row.type === "changed" || row.type === "added"}>
+        {#if row.rightSegments}
+          {#each row.rightSegments as seg, j (j)}<span class:diff-segment-changed={seg.changed}>{seg.text}</span>{/each}
+        {:else}
+          {row.rightText ?? ""}
+        {/if}
+      </div>
     </div>
   {/each}
 </div>
